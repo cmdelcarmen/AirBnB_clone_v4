6e3d125c957bafc51f9805e21b482f5f94bcaea3
luismelendez94 <2709@holbertonschool.com>
2021-10-06 04:37:37 +0000

    Task 3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  let amenities = [];
+  let idArr = [];
+  $(':checkbox').change(function() {
+    let name = $(this).attr("data-name");
+    let id = $(this).attr("data-id");
+    if (this.checked) {
+      amenities.push(name);
+      idArr.push(id);
+    } else {
+      amenities.splice($.inArray(name, amenities), 1);
+      idArr.splice($.inArray(id, idArr), 1);
+    }
+    $(".amenities > h4").text(amenities.join(', '));
+  });
+
+  $.get("http://0.0.0.0:5001/api/v1/status/", function (data) {
+    if (data.status == 'success')
+      $("div#api_status").addClass('available');
+    else
+      $("div#api_status").removeClass('available');
+  });
+});
